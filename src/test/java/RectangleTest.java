8feb715ce7390e73e84248636a034519b975944a
Imdad Ahmed <imdadah@thoughtworks.com>
2012-04-02 14:19:08 +0530

    Added more methods and tests to Rectangle & RectangleTest

--- src/test/java/RectangleTest.java
+++ src/test/java/RectangleTest.java
@@ -1,2 +1,18 @@
-public class RectangleTest extends junit.framework.TestCase {
+import org.junit.Test;
+
+import static junit.framework.Assert.assertEquals;
+
+public class RectangleTest {
+
+    @Test
+    public void shouldCalculateArea() {
+        assertEquals(10, new Rectangle(5, 2).area());
+        assertEquals(24, new Rectangle(4, 6).area());
+    }
+
+    @Test
+    public void shouldCalculatePerimeter() {
+        assertEquals(20, new Rectangle(3, 7).perimeter());
+        assertEquals(12, new Rectangle(2, 4).perimeter());
+    }
 }
